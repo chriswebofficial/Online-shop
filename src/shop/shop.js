1c6243717a44fc5e24bf9f7d3fede71dc99c33f6
chriswebofficial <christoonz221@gmail.com>
2024-09-03 07:04:31 -0700

    Initial commit

---
+++ src/shop/shop.js
@@ -0,0 +1,15 @@
+// const slider = document.querySelector('.slider');
+
+// function slideLeft() {
+//   slider.scrollBy({
+//     left: -300,
+//     behavior: 'smooth'
+//   });
+// }
+
+// function slideRight() {
+//   slider.scrollBy({
+//     left: 300,
+//     behavior: 'smooth'
+//   });
+// }
